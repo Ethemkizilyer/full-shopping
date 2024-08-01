e51aa2a55e6989d546be58eabdf823a3f4dba322
Ethemkizilyer <ethemkizilyer3546@gmail.com>
2024-08-01 16:17:02 +0300

    navbar updated

--- backend/seed.js
+++ backend/seed.js
@@ -15,13 +15,11 @@ const products =  [
       name: 'iPhone 12',
       price: 799,
       rating: 4.5,
-      thumbnail: 'https://dummyimage.com/100x100/000/fff&text=1',
+      thumbnail: 'https://cdn.vatanbilgisayar.com/Upload/PRODUCT/apple/thumb/108883-1-1_large.jpg',
       description: 'The latest iPhone with A14 Bionic chip.',
       arrivalDate: '12.10.2021',
       totalReviews: 100,
       reviews: [
-        { user: 'John Doe', comment: 'Great phone!', rating: 5 },
-        { user: 'Jane Smith', comment: 'Too expensive.', rating: 3 },
       ],
       likes: 0
     },
@@ -30,28 +28,38 @@ const products =  [
       name: 'Samsung Galaxy S21',
       price: 999,
       rating: 4.7,
-      thumbnail: 'https://dummyimage.com/100x100/000/fff&text=2',
+      thumbnail: 'https://cdn.vatanbilgisayar.com/Upload/PRODUCT/samsung/thumb/143186-1-1_large.jpg',
       description: 'The latest Samsung Galaxy with Exynos 2100.',
       arrivalDate: '15.01.2021',
       totalReviews: 150,
       reviews: [
-        { user: 'Alice Brown', comment: 'Amazing screen!', rating: 5 },
-        { user: 'Bob Johnson', comment: 'Battery life could be better.', rating: 4 },
+        { user: 'Hakan', comment: 'Süper!', rating: 5 },
       ],
       likes: 0
     },
     {
       id: 3,
-      name: 'Google Pixel 5',
+      name: 'Samsung Galaxy Z Flip6',
       price: 699,
       rating: 4.3,
-      thumbnail: 'https://dummyimage.com/100x100/000/fff&text=3',
-      description: 'The latest Google phone with pure Android experience.',
+      thumbnail: 'https://cdn.vatanbilgisayar.com/Upload/PRODUCT/samsung/thumb/146266-1-1_large.jpg',
+      description: 'Samsung Galaxy Z Flip6 512 Gb Smartphone Mint Green',
       arrivalDate: '30.09.2021',
       totalReviews: 80,
       reviews: [
-        { user: 'Chris Evans', comment: 'Stock Android is the best!', rating: 5 },
-        { user: 'Diana Prince', comment: 'Camera is not as good as expected.', rating: 3 },
+      ],
+      likes: 0
+    },
+    {
+      id: 4,
+      name: 'Xiaomi Redmi Note 12 Pro',
+      price: 500,
+      rating: 4.8,
+      thumbnail: 'https://cdn.vatanbilgisayar.com/Upload/PRODUCT/xiaomi/thumb/1-8_large.jpg',
+      description: 'Xiaomi Redmi Note 13 Pro 12GB/512GB Black Smartphone.',
+      arrivalDate: '21.04.2023',
+      totalReviews: 80,
+      reviews: [
       ],
       likes: 0
     },
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -24,8 +24,7 @@ const Navbar = () => {
   return (
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
+        <div
           class="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <img
@@ -34,9 +33,9 @@ const Navbar = () => {
             alt="Flowbite Logo"
           />
           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            Bakar
           </span>
-        </a>
+        </div>
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             {token ? (
